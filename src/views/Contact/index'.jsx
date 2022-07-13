b6d654dc8f450026828e0c2fc497c0f71fb536ef
SahilMamedov <sahilim@code.edu.az>
2022-07-14 02:45:50 +0400

    feat: styled-system

--- src/views/Contact/index'.jsx
+++ src/views/Contact/index'.jsx
@@ -1,5 +1,7 @@
 
 import { Container } from "./styled"
+import {ContactButton} from "./styled"
+
 export default function Contact () {
   return (
     <div>
@@ -7,6 +9,7 @@ export default function Contact () {
      <h1>
         Contact Page
       </h1>
+      <ContactButton bg="yellow" width={150} height={30} m="0 auto">Button</ContactButton>
      </Container>
     </div>
   )
--- src/views/Contact/styled.js
+++ src/views/Contact/styled.js
@@ -1,9 +1,13 @@
+import { space, color, layout } from "styled-system";
+import styled from "styled-components";
 
-import styled from "styled-components"
-
-export const Container=styled.div`
-margin: 0 280px;
-display:${(props)=>props.theme.display.flex};
-justify-content:${(props)=>props.theme.display.JustifyContentCenter};
-
-`
+export const Container = styled.div`
+  margin: 0 280px;
+  display: ${(props) => props.theme.display.flex};
+  justify-content: ${(props) => props.theme.display.JustifyContentCenter};
+`;
+export const ContactButton = styled.button(
+color,
+layout,
+space,
+)
